0902392995fb3ae57a6776d8a4c907becc7b4748
Mateusz Turzyński <mateusz.turzynski@goyello.com>
2017-11-07 17:36:14 +0100

    adds description to discount calculator

--- TddShop/TddShop.Cli/Order/DiscountCalculator.cs
+++ TddShop/TddShop.Cli/Order/DiscountCalculator.cs
@@ -11,11 +11,13 @@ namespace TddShop.Cli.Order
     {
         /// <summary>
         /// Possible rules:
-        ///     * Total order value > $500
-        ///     * Items from defined categories are discounted
-        ///     * Customer who has loyalty card
-        ///     * Discounts based on dates
-        ///     * RANDOM discounts in some cases
+        ///     * Total order value > $500 : 10%
+        ///     * Items from 'Beer' category are discounted: EUR 2 (don't allow negative price)
+        ///     * Items which are leftovers (only one item in stock left): 
+        ///         50% to items worth more than EUR 100, 75% to items worth more than EUR 200
+        ///     * Discounts placed in December: 30%
+        /// 
+        /// Only one discount can be applied - it should be the one which is the best for customer
         /// </summary>
         /// <param name="order"></param>
         /// <returns></returns>
