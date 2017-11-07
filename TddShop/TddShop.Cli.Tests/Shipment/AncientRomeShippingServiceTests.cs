389813ffcfb4d2dd486b84706547eb97dc146e79
mturzynskigoyello <mateusz.turzynski@goyello.com>
2017-11-07 20:54:14 +0100

    Features/ancient rome (#2)
    * ancient rome service
    
    * Homework

---
+++ TddShop/TddShop.Cli.Tests/Shipment/AncientRomeShippingServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddShop.Cli.Shipment;
+
+namespace TddShop.Cli.Tests.Shipment
+{
+    [TestFixture]
+    public class AncientRomeShippingServiceTests
+    {
+        private Mock<IDeliveryService> _deliveryService;
+        private AncientRomeShippingService _target;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _target = new AncientRomeShippingService(_deliveryService.Object);
+        }
+    }
+}
---
+++ TddShop/TddShop.Cli/Shipment/AncientRomeShippingService.cs
@@ -0,0 +1,26 @@
+﻿using TddShop.Cli.Order.Models;
+
+namespace TddShop.Cli.Shipment
+{
+    public class AncientRomeShippingService
+    {
+        private readonly IDeliveryService _deliveryService;
+
+        public AncientRomeShippingService(IDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;
+        }
+
+        /// <summary>
+        /// To ship an order you need to generate a shipment reference number (see IDeliveryService).
+        /// Ancient Rome works with romanian numbers so you will need to convert shipment reference number to a valid romanian number (string).
+        /// Use IDeliveryService to ship an order.
+        ///                
+        /// </summary>
+        /// <param name="order"></param>
+        public void ShipOrder(OrderModel order)
+        {
+            
+        }
+    }    
+}
